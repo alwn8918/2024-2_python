4b8e97adeb2e47310ee46e1fe2434e5e35f928f4
alwn8918 <alwn8918@gmail.com>
2024-12-18 01:31:52 +0900

    ✨ Feat: 분기한정법 bound 함수 구현

---
+++ branch_and_bound.py
@@ -0,0 +1,26 @@
+knapsack_weight = 16
+knapsack_amount = 4
+knapsack = [ ['n1', 40, 2], ['n2', 30, 5], ['n3', 50, 10], ['n4', 10, 5] ]
+
+def get_bound(profit, weight, level):
+    if weight >= knapsack_weight:
+        return 0
+    
+    bound = profit
+    index = level + 1
+    space = knapsack_weight
+
+    while index < knapsack_amount:
+        if knapsack[index][2] <= space:
+            bound += knapsack[index][1]
+            space -= knapsack[index][2]
+            index += 1
+        else:
+            fraction = space / knapsack[index][2]
+            bound += knapsack[index][1] * fraction
+            break
+
+    return bound
+
+
+print(get_bound(0, 0, -1))
